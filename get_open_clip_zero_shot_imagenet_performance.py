4c6519a7365dc41b870f9a975847759a0e905dfe
Temmuz Celik <tcelik@privsec0.inf.ethz.ch>
2023-11-05 13:23:14 +0100

    zeroshots

--- get_open_clip_zero_shot_imagenet_performance.py
+++ get_open_clip_zero_shot_imagenet_performance.py
@@ -18,21 +18,9 @@ OPEN_CLIP_ZERO_SHOT_IMAGENET_RESULTS_VAL = OPEN_CLIP_ZERO_SHOT_IMAGENET_RESULTS 
 
 IMAGENET1K_COUNT = 1000
 
-# %%
+from utils import create_dataloader, get_accuracy_logits_targets, get_precision_recall, \
+    get_autocast
 
-def create_dataloader(image_folder, batch_size=32, shuffle=False, num_workers=4, seed=42):
-    transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-
-    dataset = datasets.ImageFolder(image_folder, transform=transform)
-    torch.manual_seed(seed)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
-    
-    return dataloader
 # %%
 architecture_dataset = open_clip.list_pretrained()
 architecture_dataset
@@ -45,85 +33,14 @@ model, _, preprocess = open_clip.create_model_and_transforms(
     architecture, pretrained=pretrain_dataset, device=device)
 tokenizer = open_clip.get_tokenizer(architecture)
 model.eval()
+
 # %%
-import torch
-from contextlib import suppress
-
-
-def get_autocast(precision):
-    if precision == 'amp':
-        return torch.cuda.amp.autocast
-    elif precision == 'amp_bfloat16' or precision == 'amp_bf16':
-        # amp_bfloat16 is more stable than amp float16 for clip training
-        return lambda: torch.cuda.amp.autocast(dtype=torch.bfloat16)
-    else:
-        return suppress
-    
-
-
 class Args:
     def __init__(self,device):
-        self.precision = "amp"
+        self.precision = "float32"
         self.device = device
         self.batch_size = 32
-        self.model = "RN50"
 args = Args(device)
-
-# %%
-def accuracy(output, target, topk=(1,)):
-    """Computes top k accuracy
-    Args:
-        output (torch.Tensor): model output
-        target (torch.Tensor): ground truth labels
-        topk (tuple): top k values to calculate accuracy for
-    Returns:
-        list: top k accuracies"""
-    pred = output.topk(max(topk), 1, True, True)[1].t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
-    return [float(correct[:k].reshape(-1).float().sum(0, keepdim=True).cpu().numpy()) for k in topk]
-
-#%%
-def get_accuracy_logits_targets(model, classifier, dataloader, args):
-    """" Calculates accuracy, logits and targets for a given model, classifier and dataloader
-    Args:
-        model (torch.nn.Module): model to evaluate
-        classifier (torch.nn.Module): classifier to use
-        dataloader (torch.utils.data.DataLoader): dataloader to use
-        args (Args): arguments
-    Returns:
-        float: top 1 accuracy
-        float: top 5 accuracy
-        list: logits
-        list: targets
-    """
-    autocast = get_autocast(args.precision)
-    input_dtype = get_input_dtype(args.precision)
-
-    tot_targets = []
-    tot_logits = []
-    with torch.no_grad():
-        top1, top5, n = 0., 0., 0.
-        for images, target in tqdm(dataloader, unit_scale=args.batch_size):
-            images = images.to(device=args.device, dtype=input_dtype)
-            target = target.to(args.device)
-
-            with autocast():
-                # predict
-                output = model(image=images)
-                image_features = output['image_features'] if isinstance(output, dict) else output[0]
-                logits = 100. * image_features @ classifier
-
-            # measure accuracy
-            acc1, acc5 = accuracy(logits, target, topk=(1, 5))
-            top1 += acc1
-            top5 += acc5
-            n += images.size(0)
-            tot_targets.extend(target)
-            tot_logits.append(logits)
-
-    top1 = (top1 / n)
-    top5 = (top5 / n)
-    return top1, top5, tot_logits, tot_targets
 # %%
 imagenet_val_dataloader = create_dataloader(IMAGENET_VAL_FOLDER)
 # %%
@@ -143,60 +60,41 @@ top1, top5, tot_logits, tot_targets = get_accuracy_logits_targets(
     model, classifier, imagenet_val_dataloader, args)
 # %%
 tot_logits = torch.cat(tot_logits, dim=0)
-# %%
 tot_preds = tot_logits.topk(1, 1, True, True)[1].view(-1)
-# %%
 tot_targets = [tot_targets[i].reshape(1,1) for i in range(len(tot_targets))]
 tot_targets = torch.cat(tot_targets, dim=1).view(-1)
 tot_correct = tot_preds.eq(tot_targets)
-# %%
+
 tot_preds = tot_preds.cpu().numpy()
 tot_targets = tot_targets.cpu().numpy()
 tot_correct = tot_correct.cpu().numpy()
-# %% 
-true_positives = np.zeros(IMAGENET1K_COUNT)
-false_positives = np.zeros(IMAGENET1K_COUNT)
-class_count = np.zeros(IMAGENET1K_COUNT)
 
-for i in range(IMAGENET1K_COUNT):
-    true_positives[i] = tot_correct[tot_targets == i].sum()
-    false_positives[i] = (np.sum(tot_preds == i) - true_positives[i])
-    class_count[i]= (np.sum(tot_targets == i))
 # %%
-precision = true_positives / (true_positives + false_positives)
-recall = true_positives / class_count
+precision, recall = get_precision_recall(tot_preds, tot_targets)
 # %%
-def get_precision_recall(preds, targets):
-    """ Calculates precision and recall for each class
-    Args:
-        preds (np.array): predictions
-        targets (np.array): targets
-    Returns:
-        np.array: precision
-        np.array: recall"""
-    true_positives = np.zeros(IMAGENET1K_COUNT)
-    false_positives = np.zeros(IMAGENET1K_COUNT)
-    class_count = np.zeros(IMAGENET1K_COUNT)
+zeroshots = {}
+zeroshots[architecture + " " + pretrain_dataset] = {"name": precision, "surname": recall}
+zeroshots["lmp"] = {"name": "ali", "surname": "veli"}
+# %%
+zeroshots.keys()
+# %%
+zeroshots["asd"].keys()
+# %%
+import pickle 
 
-    corrects = preds == targets
-
-    for i in range(IMAGENET1K_COUNT):
-        true_positives[i] = corrects[targets == i].sum()
-        false_positives[i] = (np.sum(preds == i) - true_positives[i])
-        class_count[i]= (np.sum(targets == i))
-
-    precision = true_positives / (true_positives + false_positives)
-    recall = true_positives / class_count
-    return precision, recall
+with open('saved_dictionary.pkl', 'wb') as f:
+    pickle.dump(zeroshots, f)
+        
+with open('saved_dictionary.pkl', 'rb') as f:
+    loaded_dict = pickle.load(f)
 # %%
-a,b = get_precision_recall(tot_preds[0], tot_targets[0])
+loaded_dict
 # %%
-IMAGENET_CLASSES_SHORT = DATA_FOLDER / "imagenet_classes.txt"
+architecture
 # %%
-imagenet_classes_short = []
-with open(IMAGENET_CLASSES_SHORT, "r") as f:
-    for line in f:
-        imagenet_classes_short.append(line.strip())
+pretrain_dataset
 # %%
-imagenet_classes_short[837]
+architecture + " " + pretrain_dataset
+# %%
+zeroshots
 # %%
--- utils.py
+++ utils.py
@@ -6,10 +6,14 @@ import torch
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import sys
-import numpy as np
 import numpy.lib.format
 import struct
+from torchvision import datasets, transforms
+from contextlib import suppress
+from open_clip import get_input_dtype
+from tqdm import tqdm
 IMAGENET_LABEL_COUNT = 1000
+IMAGENET1K_COUNT = 1000
 
 #%%
 def get_top_n_indices(array : np.ndarray, n: int = 10):
@@ -583,3 +587,111 @@ def dot_products_distances(emb_A, emb_B, device=torch.device('cuda:7')):
     dot_products = torch.mm(emb_A, emb_B.t()).cpu().numpy()
     distances = torch.cdist(emb_A, emb_B).cpu().numpy()
     return dot_products, distances
+
+# %%
+
+def create_dataloader(image_folder, batch_size=32, shuffle=False, num_workers=4, seed=42):
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    dataset = datasets.ImageFolder(image_folder, transform=transform)
+    torch.manual_seed(seed)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+    
+    return dataloader
+
+# %%
+
+def get_autocast(precision):
+    if precision == 'amp':
+        return torch.cuda.amp.autocast
+    elif precision == 'amp_bfloat16' or precision == 'amp_bf16':
+        # amp_bfloat16 is more stable than amp float16 for clip training
+        return lambda: torch.cuda.amp.autocast(dtype=torch.bfloat16)
+    elif precision == 'float32':
+        return lambda: torch.cuda.amp.autocast(dtype=torch.float32)
+    else:
+        return suppress
+    
+# %%
+def accuracy(output, target, topk=(1,)):
+    """Computes top k accuracy
+    Args:
+        output (torch.Tensor): model output
+        target (torch.Tensor): ground truth labels
+        topk (tuple): top k values to calculate accuracy for
+    Returns:
+        list: top k accuracies"""
+    pred = output.topk(max(topk), 1, True, True)[1].t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+    return [float(correct[:k].reshape(-1).float().sum(0, keepdim=True).cpu().numpy()) for k in topk]
+
+#%%
+def get_accuracy_logits_targets(model, classifier, dataloader, args):
+    """" Calculates accuracy, logits and targets for a given model, classifier and dataloader
+    Args:
+        model (torch.nn.Module): model to evaluate
+        classifier (torch.nn.Module): classifier to use
+        dataloader (torch.utils.data.DataLoader): dataloader to use
+        args (Args): arguments
+    Returns:
+        float: top 1 accuracy
+        float: top 5 accuracy
+        list: logits
+        list: targets
+    """
+    autocast = get_autocast(args.precision)
+    input_dtype = get_input_dtype(args.precision)
+
+    tot_targets = []
+    tot_logits = []
+    with torch.no_grad():
+        top1, top5, n = 0., 0., 0.
+        for images, target in tqdm(dataloader, unit_scale=args.batch_size):
+            images = images.to(device=args.device, dtype=input_dtype)
+            target = target.to(args.device)
+
+            with autocast():
+                # predict
+                output = model(image=images)
+                image_features = output['image_features'] if isinstance(output, dict) else output[0]
+                logits = 100. * image_features @ classifier
+
+            # measure accuracy
+            acc1, acc5 = accuracy(logits, target, topk=(1, 5))
+            top1 += acc1
+            top5 += acc5
+            n += images.size(0)
+            tot_targets.extend(target)
+            tot_logits.append(logits)
+
+    top1 = (top1 / n)
+    top5 = (top5 / n)
+    return top1, top5, tot_logits, tot_targets
+
+def get_precision_recall(preds, targets):
+    """ Calculates precision and recall for each class
+    Args:
+        preds (np.array): predictions
+        targets (np.array): targets
+    Returns:
+        np.array: precision
+        np.array: recall"""
+    true_positives = np.zeros(IMAGENET1K_COUNT)
+    false_positives = np.zeros(IMAGENET1K_COUNT)
+    class_count = np.zeros(IMAGENET1K_COUNT)
+
+    corrects = preds == targets
+
+    for i in range(IMAGENET1K_COUNT):
+        true_positives[i] = corrects[targets == i].sum()
+        false_positives[i] = (np.sum(preds == i) - true_positives[i])
+        class_count[i]= (np.sum(targets == i))
+
+    precision = true_positives / (true_positives + false_positives)
+    recall = true_positives / class_count
+    return precision, recall
