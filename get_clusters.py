8762801544a0e89deb60f6b56d440abb9daf7654
Daniel Paleka <danepale@gmail.com>
2023-12-01 14:05:24 +0100

    add torch kmeans from some repo

--- get_clusters.py
+++ get_clusters.py
@@ -19,6 +19,8 @@ parser.add_argument('--cuda_device', type=int, default=-1,
                     help='cuda device')
 parser.add_argument('--verbose', type=int, default=0,
                     help='verbosity of kmeans fitter')
+parser.add_argument('--torch', action='store_true',
+                    help='use torch kmeans instead of scikit-learn minibatch kmeans')
 
 args = parser.parse_args()
 
@@ -33,16 +35,25 @@ embeddings = np.load(embeddings_path)
 # Find the clusters
 cluster_count = args.cluster_count
 verbose = args.verbose
-kmeans_fitter =  MiniBatchKMeans(n_clusters=cluster_count, batch_size=256 * 16, verbose=verbose, n_init=5, max_iter=500, random_state=42)
-print("Starting k-means")
-kmeans = kmeans_fitter.fit(embeddings)
+if args.torch:
+    from kmean_torch import kmeans_core
+    #km = kmeans_core(k=K,data_array=A,batch_size=3000,epochs=200)
+    km = kmeans_core(k=cluster_count, data_array=embeddings, batch_size=256 * 16, epochs=200, all_cuda=True)
+    print("Starting torch k-means")
+    km.run()
+    cluster_centers = km.cent.cpu().numpy()
+
+else:
+    kmeans_fitter =  MiniBatchKMeans(n_clusters=cluster_count, batch_size=256 * 16, verbose=verbose, n_init=5, max_iter=500, random_state=42)
+    print("Starting k-means")
+    kmeans = kmeans_fitter.fit(embeddings)
+    cluster_centers = kmeans.cluster_centers_
 
 # Save the cluster centers
 save_folder = args.save_folder
 if not os.path.exists(save_folder):
     print(f'Creating folder {save_folder}')
     os.makedirs(save_folder)
-cluster_centers = kmeans.cluster_centers_
 cluster_centers_path = os.path.join(save_folder, 'cluster_centers.npy')
 np.save(cluster_centers_path, cluster_centers)
 
@@ -56,3 +67,4 @@ dot_products_path = os.path.join(save_folder, 'dot_products.npy')
 distances_path = os.path.join(save_folder, 'distances.npy')
 np.save(dot_products_path, dot_products)
 np.save(distances_path, distances)
+
